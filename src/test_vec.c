c297153c981cc5578586dfcd7fa13d07ac3fbce0
badatkovaak <122653634+badatkovaak@users.noreply.github.com>
2024-02-20 17:23:01 +0300

    1

--- src/test_vec.c
+++ src/test_vec.c
@@ -1,10 +1,10 @@
-// #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
 #define VEC_IMPL
+#include "defines.h"
 #include "vector.h"
 
-Vec_Impl(int);
+Vec_Impl(u64);
 
 int main(void) { return EXIT_SUCCESS; }
--- src/vector.h
+++ src/vector.h
@@ -1,73 +1,61 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
+#include "defines.h"
+
 typedef struct Vector {
     void* data;
-    unsigned long len;
-    unsigned long capacity;
+    u64 len;
+    u64 capacity;
 } Vector;
 
-#endif  // !VECTOR_H
-
-#ifdef VEC_IMPL
-
+#include <stdlib.h>
 #include <string.h>
-// clang-format off
-#define Vec_Impl(type)\
-    \
-    unsigned long closest_pow_2(unsigned long num) {\
-        unsigned long res = num;\
-        while (num % 2 == 0) {\
-            num /= 2;\
-        }\
-        return res / num;\
-    }\
-    \
-    Vector create_empty_##type() {\
-    \
-    }\
-    \
-    Vector create_with_capacity_##type(unsigned long capacity) {\
-        unsigned long temp = closest_pow_2(capacity);\
-        if (temp != capacity) {\
-            temp *= 2;\
-        }\
-        void* data = malloc(temp * sizeof(type));\
-        memset(data, 0, temp * sizeof(type));\
-        Vector vec = {data, 0, temp};\
-        return vec;\
-    }\
-    \
-    void push_##type(Vector * v, type value) {\
-        if (v->capacity > v->len) {\
-            type* temp = (type *)v->data;\
-            temp[v->len] = value;\
-            v->len += 1;\
-            return;\
-        }\
-        \
-        v->data = realloc(v->data, v->len * 2);\
-        v->capacity = v->len * 2;\
-        memset((void*)((unsigned long)v->data + v->len*sizeof(type)), 0, v->len)\
-        type* temp = (type *)v->data;\
-        temp[v->len] = value;\
-        v->len += 1;\
-        return;\
-    }\
-    \
-    type pop_##type(Vector* v) {\
-        if (v->len == 0){\
-            return 0;\
-        }\
-        type* temp = (type*)v->data;\
-        type res = temp[v->len - 1];\
-        v->len--;\
-        if (v->len == 0) {\
-            v->data = NULL;\
-        }\
-        return res;\
+
+#define Vec_Impl(type)                                                    \
+    u64 closest_pow_2(u64 num) {                                          \
+        u64 res = num;                                                    \
+        while (num % 2 == 0) {                                            \
+            num /= 2;                                                     \
+        }                                                                 \
+        return res / num;                                                 \
+    }                                                                     \
+    Vector create_with_capacity_##type(u64 capacity) {                    \
+        u64 temp = closest_pow_2(capacity);                               \
+        if (temp != capacity) {                                           \
+            temp *= 2;                                                    \
+        }                                                                 \
+        void* data = malloc(temp * sizeof(type));                         \
+        memset(data, 0, temp * sizeof(type));                             \
+        Vector vec = {data, 0, temp};                                     \
+        return vec;                                                       \
+    }                                                                     \
+    void push_##type(Vector* v, type value) {                             \
+        if (v->capacity > v->len) {                                       \
+            type* temp = (type*)v->data;                                  \
+            temp[v->len] = value;                                         \
+            v->len += 1;                                                  \
+            return;                                                       \
+        }                                                                 \
+        v->data = realloc(v->data, v->len * 2);                           \
+        v->capacity = v->len * 2;                                         \
+        memset((void*)((u64)v->data + v->len * sizeof(type)), 0, v->len); \
+        type* temp = (type*)v->data;                                      \
+        temp[v->len] = value;                                             \
+        v->len += 1;                                                      \
+        return;                                                           \
+    }                                                                     \
+    type pop_##type(Vector* v) {                                          \
+        if (v->len == 0) {                                                \
+            return 0;                                                     \
+        }                                                                 \
+        type* temp = (type*)v->data;                                      \
+        type res = temp[v->len - 1];                                      \
+        v->len--;                                                         \
+        if (v->len == 0) {                                                \
+            v->data = nullptr;                                            \
+        }                                                                 \
+        return res;                                                       \
     }
 
-// clang-format on
-
-#endif  // VEC_IMPL
+#endif  // !VECTOR_H
